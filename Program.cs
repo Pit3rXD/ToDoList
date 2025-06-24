ef90cb7eeff482a206508a28fb4bbda07f6f0465
Piotr Łukaszewicz <50478926+Pit3rXD@users.noreply.github.com>
2025-06-24 19:03:43 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.IO.Pipes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+    }
+    class TaskItem
+    {
+        private string _title;
+        private DateTime _dateAdded;
+        private DateTime? _dueDate;
+        private bool _isCompleted;
+
+        public int Id { get; }
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Title cannot be empty");
+                }
+                _title = value;
+            }
+        }
+        public DateTime DateAdded { get; private set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public TaskItem(int id, string title, DateTime dateAdded, DateTime? dueDate, bool isCompelete)
+        {
+            Id = id;
+            Title = title;
+            DateAdded = dateAdded;
+            DueDate = dueDate;
+            IsCompleted = isCompelete;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"[{Id}] {Title} (added: {DateAdded:yyyy-MM-dd}, due: {DueDate?.ToString("yyyy-MM-dd") ?? "none"}, status: {(IsCompleted ? "complete" : "pending")})");
+        }
+    }
+}
---
+++ ToDoManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using ToDoList;
+
+namespace ToDoList
+{
+    internal class ToDoManager
+    {
+        public List<TaskItem> _tasks;
+        public readonly string _filePath = "tasks.json";
+        public int _lastTaskId;
+    }
+}
