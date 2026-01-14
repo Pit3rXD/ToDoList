b2900efe5637c85cb27b5c7dd8be120be1f0b463
Piotrek <piepietras@gmail.com>
2026-01-14 21:11:47 +0100

    Adding unit tests with Moq

---
+++ Services/JsonFileService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace ToDoList.Services
+{
+    internal class JsonFileService : IFileService
+    {
+        private readonly string _filePath = "tasks.json";
+
+        public List<TaskItem> Load()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<TaskItem>();
+            }
+            string json = File.ReadAllText(_filePath);
+            var tasks = JsonSerializer.Deserialize<List<TaskItem>>(json);
+
+            return tasks ?? new List<TaskItem>();
+        }
+
+        public void Save(List<TaskItem> tasks)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true}; // zapytaj czata o to
+            string json = JsonSerializer.Serialize(tasks, options);
+            File.WriteAllText(_filePath, json);
+        }
+    }
+}
---
+++ TaskItem.cs
@@ -0,0 +1,41 @@
+﻿
+namespace ToDoList
+{
+    public class TaskItem
+    {
+        private string _title = string.Empty;
+        private DateTime _dateAdded;
+        private DateTime? _dueDate;
+        private bool _isCompleted;
+
+        public int Id { get; }
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Title cannot be empty");
+                }
+                _title = value;
+            }
+        }
+        public DateTime DateAdded { get; private set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public TaskItem(int id, string title, DateTime dateAdded, DateTime? dueDate, bool isCompleted)
+        {
+            Id = id;
+            Title = title;
+            DateAdded = dateAdded;
+            DueDate = dueDate;
+            IsCompleted = isCompleted;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"[{Id}] {Title} (added: {DateAdded:yyyy-MM-dd}, due: {DueDate?.ToString("yyyy-MM-dd") ?? "none"}, status: {(IsCompleted ? "complete" : "pending")})");
+        }
+    }
+}
